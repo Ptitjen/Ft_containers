4eabc30005e453a91df0a21844a2e7a8588bc83d
Jenny Eulliot <jeulliot@k2r2p6.42mulhouse.fr>
2022-09-01 10:04:07 +0200

    equal and pair

--- srcs/equal.hpp
+++ srcs/equal.hpp
@@ -1,4 +1,34 @@
 #ifndef EQUAL_HPP
 #define EQUAL_HPP
 
+#include <exception>
+template <class InputIterator1, class InputIterator2>
+bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2) {
+  try {
+    while (first1 != last1) {
+      if (!(*first1 == *first2)) return false;
+      ++first1;
+      ++first2;
+    }
+    return true;
+  } catch (std::exception& e) {
+    throw e;
+  }
+}
+
+template <class InputIterator1, class InputIterator2, class BinaryPredicate>
+bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2,
+           BinaryPredicate pred) {
+  try {
+    while (first1 != last1) {
+      if (!pred(*first1, *first2)) return false;
+      ++first1;
+      ++first2;
+    }
+    return true;
+  } catch (std::exception& e) {
+    throw e;
+  }
+}
+
 #endif
---
+++ srcs/pair.hpp
@@ -0,0 +1,10 @@
+#ifndef PAIR_HPP
+#define PAIR_HPP
+
+template <class T1, class T2>
+struct pair {
+  T1 first;
+  T2 second;
+};
+
+#endif
